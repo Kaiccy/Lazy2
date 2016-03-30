bcd87ce8fc4eb6433f517313b653734b915bf456
afeiIOS <572161764@qq.com>
2016-03-30 14:51:46 +0800

    afei

---
+++ Lazy2/Lazy2/Classes/Scenes/SearchGoodsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchGoodsViewController.h
+//  Lazy2
+//
+//  Created by 君爵信息科技 on 16/3/30.
+//  Copyright © 2016年 Wangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchGoodsViewController : UIViewController
+
+@end
