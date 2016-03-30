0f56176be2f5ee019296b72142a7ae63692e4c5c
君爵信息科技 <junjuexinxikeji@junjuexinxikejideiMac.local>
2016-03-30 14:47:44 +0800

    wx  搜索页面搭建

---
+++ Lazy2/Lazy2/Classes/General/Views/SearchGoodsTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  SearchGoodsTableViewCell.m
+//  Lazy2
+//
+//  Created by 君爵信息科技 on 16/3/30.
+//  Copyright © 2016年 Wangxue. All rights reserved.
+//
+
+#import "SearchGoodsTableViewCell.h"
+
+@implementation SearchGoodsTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
