74a635392dc06560fb2e3336bb870d5df21bef70
君爵信息科技 <junjuexinxikeji@junjuexinxikejideiMac.local>
2016-04-06 13:23:42 +0800

    wx  支付以及支付成功页面

---
+++ Lazy2/Lazy2/Classes/Scenes/ViewController/PayWayViewController.h
@@ -0,0 +1,13 @@
+//
+//  PayWayViewController.h
+//  Lazy2
+//
+//  Created by 君爵信息科技 on 16/4/6.
+//  Copyright © 2016年 Wangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PayWayViewController : UIViewController
+
+@end
