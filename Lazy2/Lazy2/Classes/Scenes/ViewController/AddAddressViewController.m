00924afdf898b91bf6b621b55211dc4ec82b848a
afeiIOS <572161764@qq.com>
2016-04-08 16:41:08 +0800

    afei  add 个人账户

--- Lazy2/Lazy2/Classes/Scenes/ViewController/AddAddressViewController.m
+++ Lazy2/Lazy2/Classes/Scenes/ViewController/AddAddressViewController.m
@@ -10,6 +10,10 @@
 #import "MacroDefinition.h"
 @interface AddAddressViewController ()
 
+@property (nonatomic,strong)NSString *province;//省市区
+@property (nonatomic,strong)NSString *community;//小区
+@property (nonatomic,strong)AddAddressClassTableViewCell *cell;
+
 @end
 
 @implementation AddAddressViewController
@@ -54,19 +58,39 @@
 }
 #pragma mark    百度地图定位设置
 - (void)addBMKMapWay{
+    if ([[UIDevice currentDevice].systemVersion floatValue] >= 8) {
+        //由于IOS8中定位的授权机制改变 需要进行手动授权
+        CLLocationManager  *locationManager = [[CLLocationManager alloc] init];
+        //获取授权认证
+        [locationManager requestAlwaysAuthorization];
+        [locationManager requestWhenInUseAuthorization];
+    }
+    self.mapView=[[BMKMapView alloc] init];
+    [self.mapView setMapType:BMKMapTypeStandard];
+    self.mapView.zoomLevel=21;
     self.locationService=[[BMKLocationService alloc] init];
     self.locationService.delegate=self;
+    //启动LocationService
+    [self.locationService startUserLocationService];
     //普通态，以mapView为BMKMAPView对象  显示定位图层
     self.mapView.showsUserLocation=YES;
     [self.mapView updateLocationData:self.locationService.userLocation];
-    //启动LocationService
-    [self.locationService startUserLocationService];
-    //先关闭显示的定位图层
-    self.mapView.showsUserLocation=NO;
     //设置定位的状态
     self.mapView.userTrackingMode = BMKUserTrackingModeNone;
-    //显示定位图层
-    self.mapView.showsUserLocation=YES;
+}
+//用户位置更新后，会调用此函数
+- (void)didUpdateBMKUserLocation:(BMKUserLocation *)userLocation{
+    [_mapView updateLocationData:userLocation];
+    _mapView.centerCoordinate=userLocation.location.coordinate;
+    NSLog(@"现在所处经纬度为： %@",userLocation.heading);
+    NSLog(@"%@,%@",userLocation.title,userLocation.subtitle);
+}
+//实现相关delegate 处理位置信息更新 处理方向变更信息  用户方向更新后，会调用此函数
+- (void)didUpdateUserHeading:(BMKUserLocation *)userLocation{
+    [_mapView updateLocationData:userLocation];
+    _mapView.centerCoordinate = userLocation.location.coordinate;
+    NSLog(@"现在所处经纬度为： %@",userLocation.heading);
+    NSLog(@"%@,%@",userLocation.title,userLocation.subtitle);
 }
 #pragma mark    加载 TableView
 - (void)addTableViewWay{
@@ -80,18 +104,35 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return 1;
 }
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     return 360;
 }
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *CellIdentifier = @"ManageAddressTableViewClass";
-    
-    [self.addressTableView registerClass:[AddAddressClassTableViewCell class] forCellReuseIdentifier:CellIdentifier];
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    return cell;
+    static NSString *addCell = @"AddAddressClassTableViewCell";
+    self.cell= [tableView dequeueReusableCellWithIdentifier:addCell];
+    if (!self.cell) {
+        self.cell = [[AddAddressClassTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:addCell];
+        self.cell.selectionStyle = UITableViewCellSelectionStyleNone;
+        self.cell.consigneeTextField.text=@"葛飞";
+        self.cell.phoneNumberTextField.text=@"18303990502";
+        self.cell.houseNumberTextField.text=@"888";
+        [self.cell.provincialAreasButton addTarget:self action:@selector(provincialAreadButtonTouchClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.cell.locationCommunityButton addTarget:self action:@selector(locationCommunityButtonTouchClick) forControlEvents:UIControlEventTouchUpInside];
+        [self.cell.saveAddress addTarget:self action:@selector(saveAddressButtonTouchClick) forControlEvents:UIControlEventTouchUpInside];
+    }
+    return self.cell;
+}
+#pragma  mark   添加地址cell的点击事件
+- (void)provincialAreadButtonTouchClick{
+    //定位省市区
+    self.cell.provincialAreasTextField.text=self.province;
+}
+- (void)locationCommunityButtonTouchClick{
+    //定位小区
+    self.cell.locationCommunityTextField.text=self.community;
+}
+- (void)saveAddressButtonTouchClick{
+    //保存所添加的地址
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ Lazy2/Lazy2/Classes/Scenes/ViewController/MyAccountCenterViewController.h
@@ -0,0 +1,17 @@
+//
+//  MyAccountCenterViewController.h
+//  Lazy2
+//
+//  Created by yinqijie on 16/4/8.
+//  Copyright © 2016年 Wangxue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAccountCenterViewController : UIViewController
+
+    /*
+        我的账号
+     */
+
+@end
